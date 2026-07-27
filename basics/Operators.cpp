18fca8eef8bec5849b8688dc692907b31432ed70
albashzia <ahmedalbashzia@gmail.com>
2026-07-27 17:08:55 +0500

    Add a new file named Operators.cpp and practice the basic operators

---
+++ basics/Operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Sum of "<<a<<" and "<<b<<" is "<<a + b;
+    cout<<"Difference of "<<a<<" and "<<b<<" is "<<a - b;
+    cout<<"Product of "<<a<<" and "<<b<<" is "<<a * b;
+    cout<<"Quotient of "<<a<<" is "<<b/a;
+}
