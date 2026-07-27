487d45864b5074fd4b8b060aaf11149415c34e8a
albashzia <ahmedalbashzia@gmail.com>
2026-07-27 17:10:14 +0500

    Add end of line statements to the Operators.cpp file for clear display on console

--- basics/Operators.cpp
+++ basics/Operators.cpp
@@ -7,7 +7,10 @@ int main() {
     cout << "Enter second number: ";
     cin >> b;
     cout << "Sum of "<<a<<" and "<<b<<" is "<<a + b;
+    cout << endl;
     cout<<"Difference of "<<a<<" and "<<b<<" is "<<a - b;
+    cout << endl;
     cout<<"Product of "<<a<<" and "<<b<<" is "<<a * b;
+    cout << endl;
     cout<<"Quotient of "<<a<<" is "<<b/a;
 }
