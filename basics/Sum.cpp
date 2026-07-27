571f7251e2d21ea54efc4f7c6aa9a4dcb8b837af
albashzia <ahmedalbashzia@gmail.com>
2026-07-27 17:02:34 +0500

    Change the value intake from 1 statement to 2 separate statements

--- basics/Sum.cpp
+++ basics/Sum.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
     cout << "Sum of " << a << " and " << b << " is: " << a + b << endl;
 }
