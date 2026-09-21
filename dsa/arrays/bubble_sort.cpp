6904a3b9e4a1b888fb8f4c576233d760e163e56a
albashzia <ahmedalbashzia@gmail.com>
2026-09-21 20:43:42 +0500

    Add a basic bubble_sort program for arrays

--- dsa/arrays/bubble_sort.cpp
+++ dsa/arrays/bubble_sort.cpp
@@ -1,3 +1,18 @@
-//
-// Created by Laptop House on 9/19/2026.
-//
+#include<iostream>
+using namespace std;
+int main() {
+    int n = 7;
+    int a[7] = {12,7,9,2,20,3,8};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (a[j]>a[j+1]) {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for (int k = 0; k < sizeof(a)/sizeof(a[0]); k++) {
+        cout<<a[k]<<" ";
+    }
+}
